e5a90168d65f59a7ae1cedd929d8142508b44911
adyuer de jesus ojeda badel <162822301+AdyuerHack@users.noreply.github.com>
2025-11-25 21:19:57 -0500

    Delete taller_P1 directory

--- taller_P1/moviereviewrs/moviereviews/recs/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RecsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'recs'
--- taller_P1/moviereviewrs/moviereviews/recs/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import RecommendView
-
-app_name = "recs"
-
-urlpatterns = [
-    path("", RecommendView.as_view(), name="recommend"),
-]
